0e71a6799ab8b449a4d379452e73d62813c023ff
SvetlanaMartynovaD <120434915+SvetlanaMartynovaD@users.noreply.github.com>
2025-04-26 10:10:16 +1000

    + category

--- data/products.py
+++ data/products.py
@@ -8,6 +8,8 @@ class Product(SqlAlchemyBase):
 
     prod_id = sqlalchemy.Column(sqlalchemy.Integer, primary_key=True, autoincrement=True)
     prod_name = sqlalchemy.Column(sqlalchemy.String(100), nullable=False)
+    prod_volume = sqlalchemy.Column(sqlalchemy.String(7), nullable=False)
+    prod_category = sqlalchemy.Column(sqlalchemy.String(30), nullable=False)
     price = sqlalchemy.Column(sqlalchemy.Numeric(10, 2), nullable=False)
     description = sqlalchemy.Column(sqlalchemy.Text, nullable=True)
     img_prod = sqlalchemy.Column(sqlalchemy.Text, nullable=True)
