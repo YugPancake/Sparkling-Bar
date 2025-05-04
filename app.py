81fd8e47b7ac520c4fbb825bcc8456a0bbe93ce4
YugPancake <yug.blet@gmail.com>
2025-05-05 09:32:14 +1000

    добавлен логин менеджер

--- app.py
+++ app.py
@@ -2,9 +2,18 @@ from flask import Flask, render_template
 from datetime import datetime
 from data import db_session
 from data.users import User
+from flask_login import LoginManager, login_user, logout_user, login_required, current_user
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'secret_key'
+
+login_manager = LoginManager(app)
+login_manager.login_view = 'login'
+
+@login_manager.user_loader
+def load_user(user_id):
+    db_sess = db_session.create_session()
+    return db_sess.query(User).filter(User.id == user_id).first()
 
 @app.route('/', methods=['GET', 'POST'])
 def home():   
