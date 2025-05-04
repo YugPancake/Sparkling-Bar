11b441e7df52129166cd3e928d78d33bcc81fe71
YugPancake <yug.blet@gmail.com>
2025-05-05 09:30:33 +1000

    добавлен рут на главную и пустой файл для верстки главной

--- app.py
+++ app.py
@@ -6,9 +6,11 @@ from data.users import User
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'secret_key'
 
-def main():
+@app.route('/', methods=['GET', 'POST'])
+def home():   
+    return render_template('index.html', title="Главная страница")
+    
+if __name__ == '__main__':
     db_session.global_init("db/bar.db")
     app.run()    
-
-if __name__ == '__main__':
-    main()  
+ 
