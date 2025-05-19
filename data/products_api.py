7f7940753a320ad2bab85ca92036d91d83ea4645
YugPancake <yug.blet@gmail.com>
2025-05-19 16:03:43 +1000

    фильтрация каталога

--- data/products_api.py
+++ data/products_api.py
@@ -23,3 +23,53 @@ def get_products():
 
     products_list = [product.to_dict() for product in products]
     return jsonify({'products': products_list})
+
+@blueprint.route('/api/catalog', methods=['GET'])
+def filter_catalog():
+    db_sess = db_session.create_session()
+    category_map = {
+        "snacks": "Закуски",
+        "cocktails": "Коктейли",
+        "bitters": "Настойки",
+        "non_alcoholic": "Безалкогольное"
+    }
+
+    requested_categories = request.args.getlist('category')
+    categories = [category_map[cat] for cat in requested_categories if cat in category_map]
+
+    min_price = request.args.get('min_price', type=float)
+    max_price = request.args.get('max_price', type=float)
+    
+    order_by = request.args.get('order_by', default=None, type=str)
+
+    query = db_sess.query(Product)
+
+    print(f"Requested Categories: {requested_categories}")
+    print(f"Mapped Categories: {categories}")
+    print(f"Min Price: {min_price}, Max Price: {max_price}")
+    print(f"Order By: {order_by}")
+
+    if categories:
+        query = query.filter(Product.prod_category.in_(categories))
+
+    if min_price is not None:
+        query = query.filter(Product.price >= min_price)
+    if max_price is not None:
+        query = query.filter(Product.price <= max_price)
+
+    if order_by == 'popular':
+        query = query.order_by(Product.prod_id.asc())
+    elif order_by == 'cheaper':
+        query = query.order_by(Product.price)
+    elif order_by == 'expensive':
+        query = query.order_by(Product.price.desc())
+    elif order_by == 'new':
+        query = query.order_by(Product.prod_id.desc())
+
+    products = query.all()
+    
+    print(f"Found Products: {len(products)}")
+    
+    products_data = [product.to_dict() for product in products]
+
+    return jsonify({'success': True, 'products': products_data})
