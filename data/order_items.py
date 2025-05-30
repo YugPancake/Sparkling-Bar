340d5bf4a583458e333ab7b324e68cfdc751fc49
YugPancake <yug.blet@gmail.com>
2025-05-30 10:26:07 +1000

    миграция

--- data/order_items.py
+++ data/order_items.py
@@ -6,11 +6,24 @@ class OrderItem(SqlAlchemyBase):
     __tablename__ = 'order_items'
 
     item_id = sqlalchemy.Column(sqlalchemy.Integer, primary_key=True, autoincrement=True)
-    item_order_id = sqlalchemy.Column(sqlalchemy.Integer, sqlalchemy.ForeignKey('orders.o_id'), nullable=False)
+    item_order_id = sqlalchemy.Column(sqlalchemy.Integer, sqlalchemy.ForeignKey('orders.o_id'), nullable=True)
     item_prod_id = sqlalchemy.Column(sqlalchemy.Integer, sqlalchemy.ForeignKey('products.prod_id'), nullable=False)
     item_user_id = sqlalchemy.Column(sqlalchemy.Integer, sqlalchemy.ForeignKey('users.user_id'), nullable=False)
+    item_amount = sqlalchemy.Column(sqlalchemy.Integer, nullable=False, default=1)
 
     item_order = orm.relationship('Order')
     item_prod = orm.relationship('Product')
     item_user = orm.relationship('User', back_populates='order_items')
+    
+    def to_dict(self, only=None):
+        data = {
+            "item_id": self.item_id,
+            "item_order_id": self.item_order_id,
+            "item_prod_id": self.item_prod_id,
+            "item_user_id": self.item_prod_id,
+            "item_amount": self.item_prod_id
+        }
+        if only:
+            return {key: data[key] for key in only if key in data}
+        return data
 
--- data/orders.py
+++ data/orders.py
@@ -8,10 +8,22 @@ class Order(SqlAlchemyBase):
 
     o_id = sqlalchemy.Column(sqlalchemy.Integer, primary_key=True, autoincrement=True)
     user_id = sqlalchemy.Column(sqlalchemy.Integer, sqlalchemy.ForeignKey('users.user_id'), nullable=False)
-    o_sum = sqlalchemy.Column(sqlalchemy.Numeric(10, 2), nullable=False)  # DECIMAL(10,2)
-    o_status = sqlalchemy.Column(sqlalchemy.Enum('выполнен', 'готовится', 'обрабатывается'), nullable=False)  # ENUM
+    o_sum = sqlalchemy.Column(sqlalchemy.Numeric(10, 2), nullable=False)
+    o_status = sqlalchemy.Column(sqlalchemy.Enum('выполнен', 'готовится', 'обрабатывается'), nullable=False) 
     o_date = sqlalchemy.Column(sqlalchemy.DateTime, default=datetime.datetime.now)
 
     user = orm.relationship('User')
 
     order_items = orm.relationship('OrderItem', back_populates="item_order")
+    
+    def to_dict(self, only=None):
+        data = {
+            "o_id": self.o_id,
+            "user_id": self.user_id,
+            "o_sum": self.o_sum,
+            "o_status": self.o_status,
+            "o_date": self.o_date
+        }
+        if only:
+            return {key: data[key] for key in only if key in data}
+        return data
